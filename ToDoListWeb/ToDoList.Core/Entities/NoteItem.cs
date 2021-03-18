a3f078a31201f937d743e0baf480ceff681e547e
irinakadushkina <kadushkina.i@list.ru>
2021-03-18 20:35:23 +0300

    small fix

--- ToDoListWeb/ToDoList.Core/Entities/NoteItem.cs
+++ ToDoListWeb/ToDoList.Core/Entities/NoteItem.cs
@@ -1,10 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using ToDoList.Core.Entities.Base;
 
 namespace ToDoList.Core.Entities
 {
-    class NoteItem
+    enum StatusType { ToDo, InProgess, Completed }
+
+    public class NoteItem : Entity
     {
+        public NoteItem()
+        {
+
+        }
+        public DateTime DateCreated { get; set; }
+        public string NoteContent { get; set; }
+        public string NoteDescription { get; set; }
+        public int NoteIndex { get; set; }
+        StatusType Status { get; set; }
+        public Guid NoteId { get; set; }
     }
 }
+
+
